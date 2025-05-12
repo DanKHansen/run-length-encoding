3bbb610bb3ece64f8ee044b8ff46529f2937959b
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-12 11:26:02 +0200

    refactor

--- src/main/scala/RunLengthEncoding.scala
+++ src/main/scala/RunLengthEncoding.scala
@@ -7,9 +7,7 @@ object RunLengthEncoding:
          case Nil => acc
          case _   =>
             val (first, rest) = cs.span(_ == cs.head)
-            loop(
-              rest.dropWhile(_ == cs.head),
-              acc + (if first.length > 1 then first.length.toString else "") + first.head)
+            loop(rest, acc + (if first.length > 1 then first.length.toString else "") + first.head)
       loop(s.toList, "")
 
    def decode(s: String): String =
@@ -18,6 +16,5 @@ object RunLengthEncoding:
          case Nil => acc
          case _   =>
             val (digits, rest) = cs.span(_.isDigit)
-            val count = if digits.isEmpty then 1 else digits.mkString.toInt
-            loop(rest.drop(1), acc + rest.head.toString * count)
+            loop(rest.drop(1), acc + rest.head.toString * (if digits.isEmpty then 1 else digits.mkString.toInt))
       loop(s.toList, "")
