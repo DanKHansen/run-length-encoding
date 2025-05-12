464ade336b58b307b58ca84785c30d105a06b323
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-12 11:53:20 +0200

    refactor

--- src/main/scala/RunLengthEncoding.scala
+++ src/main/scala/RunLengthEncoding.scala
@@ -1,6 +1,6 @@
 object RunLengthEncoding:
    def encode(s: String): String =
-      ("(.)\\1*".r findAllIn s).map(g => if g.length > 1 then g.length.toString + g.head else g.head).mkString
+      ("(.)\\1*".r findAllIn s).map(g => s"${if g.length > 1 then g.length else ""}${g.head}").mkString
 
    def decode(s: String): String =
       ("(\\d*)(\\D)".r findAllMatchIn s).map(m => m.group(2) * m.group(1).toIntOption.getOrElse(1)).mkString
